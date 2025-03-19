547144690727295f1b2806c882b051d42da3f1e1
mariaeduardamachado <mariaeduarda0450@gmail.com>
2025-03-19 09:15:17 -0300

    sintaxe

---
+++ comandos_iniciais/sintaxe_py.py
@@ -0,0 +1,28 @@
+if 5 > 2:
+  print("Cinco é maior que dois!")
+
+x = 5 
+y = "Hello, World!"
+print(x)
+print(y)
+
+x = 4
+x = "Sally"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
